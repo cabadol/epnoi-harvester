26eead5cf8089d31b0c13ae50740b198aed698a6
Carlos Badenes <cbadenes@gmail.com>
2015-03-18 15:32:41 +0100

    version 1.0.0

---
+++ src/main/groovy/routes.groovy
@@ -0,0 +1,41 @@
+import es.upm.oeg.epnoi.harvester.AbstractRouteBuilder
+import org.apache.camel.LoggingLevel
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+class routes extends AbstractRouteBuilder{
+
+    protected static final Logger LOG = LoggerFactory.getLogger(routes.class);
+
+    @Override
+    public void configure() throws Exception {
+        super.configure()
+
+        /*********************************************************************************************************************************
+         * ROUTE 1: RSS
+         *********************************************************************************************************************************/
+        from("file:"+basedir+"/rss?recursive=true&include=.*\\.xml&doneFileName=\${file:name}.done").
+                to("direct:setCommonRssXpathExpressions").
+                to("seda:notifyUIA")
+
+
+        /*********************************************************************************************************************************
+         * ROUTE 2: OAIPMH
+         *********************************************************************************************************************************/
+        from("file:"+basedir+"/oaipmh?recursive=true&include=.*\\.xml&doneFileName=\${file:name}.done").
+                to("direct:setCommonOaipmhXpathExpressions").
+                to("seda:notifyUIA")
+
+
+        /*********************************************************************************************************************************
+         * -> To UIA
+         *********************************************************************************************************************************/
+        from("seda:notifyUIA").
+                process(contextGenerator).
+                log(LoggingLevel.INFO,LOG,"File Read: '\${header.CamelFileName}'").
+                //to("euia:out?servers="+ uiaServers)
+                to("stream:out")
+
+    }
+
+}
