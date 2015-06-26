0b4a629d0215a91c8ba5b0fe4feb5c9566e30552
Carlos Badenes <cbadenes@gmail.com>
2015-06-26 17:11:32 +0200

    print a table with corpus information

--- src/test/java/es/upm/oeg/epnoi/harvester/domain/CorpusList.java
+++ src/test/java/es/upm/oeg/epnoi/harvester/domain/CorpusList.java
@@ -1,6 +1,8 @@
 package es.upm.oeg.epnoi.harvester.domain;
 
+import com.google.common.base.CharMatcher;
 import com.google.gson.Gson;
+import org.apache.commons.lang.StringUtils;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -15,7 +17,7 @@ public class CorpusList {
 
     public static void main(String[] args) throws FileNotFoundException {
 
-        File directory = new File("/Users/cbadenes/Projects/epnoi-ressist/src/test/resources/json");
+        File directory = new File("/Users/cbadenes/Temp/epnoi-harvester-1.0.5/researchObjects/all");
         Gson gson = new Gson();
 
 
@@ -26,19 +28,34 @@ public class CorpusList {
             StringBuilder description = new StringBuilder();
             //\textit{source} 			& $0\%$ 		& $93.19\%$ 	& $0\%$ 		& $1.83\%$ 	& $0\%$ 		& $4.96\%$ 	& $0\%$  \\ \hline
 
-            description.append("\\textit{").append(ro.getUri()).append("}").
-                    append("\t& ").append(ro.getMetainformation().getTitle().replace("&","and").replace("\n"," ")).
-                    append("\t& ").append(ro.getMetainformation().getDescription().replace("&","and").replace("\n"," "));
+            description.
+                    append("").append(StringUtils.substringBefore(StringUtils.substringAfterLast(ro.getSource().getUrl(), "index.php/"), "/").toUpperCase()).
+                    append("\t&\\textit{").append(ro.getUri()).append("}").
+                    append("\t& ").append(ro.getMetainformation().getTitle().replace("&", "and").replace("\n", " ").replace("%", "\\%"));
+//                    .append("\t& ").append(CharMatcher.ASCII.retainFrom(ro.getMetainformation().getDescription()
+//                        .replace("&", "and").replace("\n", " ").replace("\r", " ")
+//                        .replace("%", "\\%").replace("{", "\\{").replace("}","\\}")));
 
 
             StringBuilder authors = new StringBuilder();
-            authors.append("\\begin{itemize}");
+            authors.append("\\pbox{3cm}{");
             for(Creator creator: ro.getMetainformation().getCreators()){
 
-                authors.append("\\item ").append(creator.getSurname().trim()).append(",").append(creator.getName());
 
+
+                authors
+//                        .append("\\pbox{20cm}{")
+                        .append("* ")
+                        .append(StringUtils.stripStart(creator.getSurname().trim()
+                                .replace("&", "and").replace("\n", " ").replace("\r", " ")
+                                .replace("%", "\\%").replace("{", "\\{").replace("}", "\\}")," "))
+                        .append(",")
+                        .append(StringUtils.stripStart(creator.getName()
+                                .replace("&", "and").replace("\n", " ").replace("\r", " ")
+                                .replace("%", "\\%").replace("{", "\\{").replace("}", "\\}")," "))
+                                .append(" \\\\ ");
             }
-            authors.append("\\end{itemize}");
+            authors.append("}");
             description.append("\t& ").append(authors).append("\\\\ \\hline");
 
             System.out.println(description.toString());
